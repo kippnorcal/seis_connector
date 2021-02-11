d3ae7ba257e8b83b1e28fbfc36c5a4e4f5864029
Susan Xiong <48801993+susanxiong@users.noreply.github.com>
2021-02-10 16:03:21 -0800

    PROJ21-1546 Update download function and add some documentation

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import logging
 import traceback
 import os
+
 import pandas as pd
 from sqlsorcery import MSSQL
-import config
-from mailer import Mailer
-from ftp import FTP
 
-# Pull file from FTP to local
-# Confirm that files have been downloaded; if not, throw an error
-# Archive files in FTP
-# Transform files from FTP into dataframe
-# [TBD] Compare file from FTP to what's in our database; write errors somewhere
-# Import into database
+import config
+from ftp import FTP
+from mailer import Mailer
 
 class Connector:
     """
@@ -25,17 +20,24 @@ class Connector:
         self.ftp = FTP()
         self.localdir = "files"
         self.remotedir = "seis"
-
-    #Must include 'self' in the paranthesis so you can call anything in the class earlier
-    def get_files_from_ftp(self):
-        filelist = [f for f in os.listdir(self.localdir)]
-        for f in filelist:
-            os.remove(os.path.join(self.localdir, f))
-
-        self.ftp.download_all_files(self.remotedir,self.localdir)
         
-        self.filenames = os.listdir(self.localdir)
-        logging.info(f"{len(self.filenames)} files downloaded")
+    def remove_local_files(self):
+        """
+        Remove any leftover files from local project directory.
+        """
+        filelist = [f for f in os.listdir(self.localdir)]
+        for filename in filelist:
+            if "gitkeep" not in filename: 
+                os.remove(os.path.join(self.localdir, filename))
+
+    def get_files_from_ftp(self):
+        """
+        Loop through folders and download files from FTP.
+        """
+        self.remove_local_files()
+        self.ftp.download_all(self.remotedir, self.localdir, config.file_names())
+        self.filenames = [f for f in os.listdir(self.localdir) if f.endswith(".csv")]
+        logging.info(f"{len(self.filenames)} files downloaded. ")
     
     def read_into_df(self):
         dfs = []
@@ -56,8 +58,15 @@ def main():
     config.set_logging()
     connector = Connector()
     connector.get_files_from_ftp()
-    connector.read_into_df()
-    connector.archive_files()
+    # connector.read_into_df()
+    # connector.archive_files()
+
+    # Pull file from FTP to local
+    # Confirm that files have been downloaded; if not, throw an error
+    # Archive files in FTP
+    # Transform files from FTP into dataframe
+    # [TBD] Compare file from FTP to what's in our database; write errors somewhere
+    # Import into database
 
 if __name__ == "__main__":
     try:
