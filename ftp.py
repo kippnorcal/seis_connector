44be90f27624a44adab7720691266b2f218e9211
Mark Young <markyoung.ca@gmail.com>
2024-09-28 10:15:48 -0700

    Trimmed down FTP class

--- ftp.py
+++ ftp.py
@@ -1,10 +1,13 @@
 import os
+from typing import List
+
 import pysftp
 
 
 FTP_HOST = os.getenv("FTP_HOST")
 FTP_USER = os.getenv("FTP_USER")
 FTP_PWD = os.getenv("FTP_PWD")
+
 
 class FTP:
     """
@@ -23,9 +26,8 @@ class FTP:
             password=FTP_PWD,
             cnopts=self.cnopts,
         )
-        self.file_names = ["Student.csv", "Service.csv"]
 
-    def get_directory_names(self, remotedir):
+    def get_directory_names(self, remote_dir_path: str) -> List[str]:
         """
         Return a list of all the directory names. For SEIS, the directory names are the school names.
 
@@ -35,10 +37,9 @@ class FTP:
         Return:
             list: string names of the directories.
         """
-        self.directory_names = self.ftpsrv.listdir(remotedir)
-        return self.directory_names
+        return self.ftpsrv.listdir(remote_dir_path)
 
-    def download_all(self, remotedir, localdir):
+    def download_all(self, remote_dir_path: str, local_dir_path: str, sub_dir_names: List[str], file_names: List[str] -> None):
         """
         Loop through all sub-directories and download the files defined in init.
 
@@ -49,55 +50,10 @@ class FTP:
         Return:
             none
         """
-        for school in self.directory_names:
-            for file_name in self.file_names:
+        for school in sub_dir_names:
+            for file_name in file_names:
                 self.ftpsrv.get(
-                    f"{remotedir}/{school}/{file_name}",
-                    f"{localdir}/{school}_{file_name}",
+                    f"{remote_dir_path}/{school}/{file_name}",
+                    f"{local_dir_path}/{school}_{file_name}",
                     preserve_mtime=True,
                 )
-
-    def _archive_file(self, file):
-        """
-        Place the file in an 'archive' folder within its directory.
-
-        Params:
-            file (str): path and name of the file that will be archived.
-
-        Return:
-            none
-        """
-        self.ftpsrv.rename(
-            file, file.replace(self.remotedir, f"{self.remotedir}/archive")
-        )
-
-    def _do_nothing(self, file):
-        """
-        Used for files and unknown file types.
-
-        Params:
-            file (str): path and name of the file that will be ignored.
-
-        Return:
-            none
-        """
-        pass
-
-    def archive_remote_files(self, remotedir):
-        """
-        Archive all of the files in the specified remote directory.
-
-        Params:
-            file (str): path of the remote directory (ie. the FTP directory)
-
-        Return:
-            none
-        """
-        self.remotedir = remotedir
-        self.ftpsrv.walktree(
-            self.remotedir,
-            fcallback=self._archive_file,
-            dcallback=self._do_nothing,
-            ucallback=self._do_nothing,
-            recurse=False,
-        )
